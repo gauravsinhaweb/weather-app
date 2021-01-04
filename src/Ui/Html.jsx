d0e8a224d616c706eaa64aeffc52a8bec59642e2
gauravsinha18 <59147380+gauravsinha18@users.noreply.github.com>
2021-01-04 17:54:04 +0530

    'dep'

--- src/Ui/Html.jsx
+++ src/Ui/Html.jsx
@@ -4,16 +4,8 @@ import MyLocationIcon from "@material-ui/icons/MyLocation";
 import Footer from "./Wi/Footer";
 import Bottom from "./Wi/Bottom";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import { createMuiTheme } from "@material-ui/core/styles";
 import Ts from "./Wi/Thunderstorm";
-const theme = createMuiTheme({
-  MuiTextField: {
-    InputLabelProps: { shrink: true },
-  },
-});
-const inputProps = {
-  step: 300,
-};
+
 function Html() {
   const [location, setLocation] = useState(null);
   const [city, setCity] = useState("New Delhi");
@@ -38,57 +30,53 @@ function Html() {
               setCity(e.target.value);
             }}
             placeholder="Search Location"
-            id="outlined-basic"
-            variant="outlined"
           />
           <span className="icon">
             <MyLocationIcon />
           </span>
         </div>
       </div>
+      <Bottom />
 
+      <Footer />
       {!location ? (
         <p className="nodata">no data found </p>
       ) : (
-        <div className="parent">
-          <div className="text">
-            <div className="city">
-              {city}, IN
-              <span className="pin">
-                <LocationOnOutlinedIcon />
-              </span>
-            </div>
-            <div className="temp">{Math.round(`${location.temp}`)}°C</div>
-            <div>
-              <Ts />
-            </div>
-            <div className="desc">Haze,</div>
-            <div className="pressure">Pressure:</div>
-            <div className="feelpressure">{location.pressure / 100} Pa</div>
-
-            <div className="date">
-              Thu Dec 31 2020,
-              <br />
-              22:14:28
-            </div>
-
-            <div className="feelslike">Feels Like: </div>
-            <div className="feeltemp">{location.feels_like}°C</div>
-
-            <div className="left">
-              <span className="dawn">Dawn: </span>
-              <span className="bold">06:00 Hrs</span>
-            </div>
-            <div className="right">
-              <span className="dusk">Dusk:</span>
-              <span className="bold">18:00 Hrs</span>
-            </div>
-            <div className="copyright">
-              Copyright ©2021 All rights reserved. @GAURAV.SINHA
-            </div>
+        <div className="text">
+          <div className="city">
+            {city}, IN
+            <span className="pin">
+              <LocationOnOutlinedIcon />
+            </span>
           </div>
-          <Bottom />
-          <Footer />
+          <div className="temp">{Math.round(`${location.temp}`)}°C</div>
+          <div>
+            <Ts />
+          </div>
+          <div className="desc">Haze,</div>
+          <div className="pressure">Pressure:</div>
+          <div className="feelpressure">{location.pressure / 100} Pa</div>
+
+          <div className="date">
+            Thu Dec 31 2020,
+            <br />
+            22:14:28
+          </div>
+
+          <div className="feelslike">Feels Like: </div>
+          <div className="feeltemp">{location.feels_like}°C</div>
+
+          <div className="left">
+            <span className="dawn">Dawn: </span>
+            <span className="bold">06:00 Hrs</span>
+          </div>
+          <div className="right">
+            <span className="dusk">Dusk:</span>
+            <span className="bold">18:00 Hrs</span>
+          </div>
+          <div className="copyright">
+            Copyright ©2021 All rights reserved. @GAURAV.SINHA
+          </div>
         </div>
       )}
     </>
